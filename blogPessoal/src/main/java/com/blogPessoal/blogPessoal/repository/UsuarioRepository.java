a7835f3fba1e891e3ac73a67a8df9f26ec1932fa
larissa4lves <p4ndap0wer24@gmail.com>
2021-04-13 10:52:55 -0300

    subindo arquivos spring

---
+++ blogPessoal/src/main/java/com/blogPessoal/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.blogPessoal.blogPessoal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blogPessoal.blogPessoal.model.Usuario;
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Optional <Usuario>findByUsuario(String usuario);
+
+
+}
